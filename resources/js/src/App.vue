f2e3d71d6d7a6ea89a4ef73115d6e293f54bbb86
teguhmuhams <teguhmuhams@gmail.com>
2023-07-11 07:44:04 +0700

    Add yoast SEO

---
+++ resources/js/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <vue-yoast-component></vue-yoast-component>
+</template>
+
+<script>
+import VueYoastComponent from '../components/VueYoastComponent.vue'
+
+export default {
+    components: { VueYoastComponent },
+    name:"App"
+}
+</script>
+
+<style scoped>
+</style>
